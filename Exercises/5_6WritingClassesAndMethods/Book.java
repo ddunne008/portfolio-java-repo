c940b945935799dd1e5fa8b4db26b6dee5c69c99
ddunne008 <d.dunne2447@student.leedsbeckett.ac.uk>
2025-03-09 20:10:53 +0000

    Update 3

--- Exercises/5_6WritingClassesAndMethods/Book.java
+++ Exercises/5_6WritingClassesAndMethods/Book.java
@@ -1,33 +1,36 @@
 import java.util.Scanner;
 
-
 public class Book {
     String tittle;
     String author;
     String publisher;
     int copyRightDate;
-    String output;
 
     @Override
     public String toString() {
-        output += ("Author" + author);
-        output += ("Title" + tittle);
-        output += ("Publisher" + publisher);
-        output += ("Copyright Date" + copyRightDate);
-        return output;
+        StringBuilder output = new StringBuilder();
+        output.append("Author").append(author);
+        output.append("Title").append(tittle);
+        output.append("Publisher").append(publisher);
+        output.append("Copyright Date").append(copyRightDate);
+        return output.toString();
     }
 
     public static class BookInfo {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Enter title: ");
-            String tittle = sc.nextLine();
+            Book BookDetails = new Book();
+
+            System.out.print("Enter tittle: ");
+            BookDetails.tittle = sc.nextLine();
             System.out.print("Enter author: ");
-            String author = sc.nextLine();
+            BookDetails.author = sc.nextLine();
             System.out.print("Enter publisher: ");
+            BookDetails.publisher = sc.nextLine();
+
             sc.close();
 
-            Book BookDetails = new Book();
+
             System.out.println(BookDetails);
         }
     }
